cf9079ea4f334bc4af7ca633a109fad21d8d8f0e
ECH <ech@MacBook-Pro-de-ECH.local>
2026-03-31 11:10:15 -0600

    Exclude conversation routes from CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'webhooks/*',
+            'conversation/*',
+            'test/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
